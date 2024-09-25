35e6be4749e5439cd88bcabe79fdd06108a5e5d4
Michael Billmire <mgbillmi@mtu.edu>
2024-09-25 14:11:04 -0400

    Adds output syncing cron script

---
+++ data_management_scripts/cron/sync_cdr_output_to_outputlayer_cron.py
@@ -0,0 +1,17 @@
+import os, pidfile, sys
+from data_management_scripts import dm_util
+
+
+# First check if there's an existing sync'ing process running; exit if so.
+pid_current = int(os.getpid())
+pids = []
+for line in os.popen(f'ps aux | grep {__file__}'):
+   if 'grep' in line or 'aux' in line:
+      continue
+   pid = int(line.split()[1])
+   if pid != pid_current:
+      print('Sync process already running; exiting.')
+      sys.exit(0)
+
+# Run sync
+dm_util.util.sync_cdr_prospectivity_outputs_to_outputlayer()
