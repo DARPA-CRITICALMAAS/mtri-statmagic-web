a7f8c6804d26701e19b0964207a0785afd09f035
aplyons <aplyons@mtu.edu>
2025-01-17 10:08:17 -0500

    Sets up logger in Django settings

--- statmagic/settings.py
+++ statmagic/settings.py
@@ -45,6 +45,31 @@ URL_PREFIX = '/' if IS_MTRI_SERVER else '/statmagic/'
 
 ALLOWED_HOSTS = ['*']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s '
+                      '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
+
 
 # Application definition
 
